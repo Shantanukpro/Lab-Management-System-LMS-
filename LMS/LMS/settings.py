039256e5fb0918c027f6d542aa404f75cb44750f
Shantanukpro <shantanukpro@gmail.com>
2025-09-17 00:26:46 +0530

    Database setup

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'labs',
 ]
 
 MIDDLEWARE = [
@@ -76,11 +77,14 @@ from decouple import config
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', cast=int),
+        'NAME': 'lms_db',
+        'USER': 'root',
+        'PASSWORD': 'Lenovo#25',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
