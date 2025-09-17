23ea99c6054d84e257b03d9e2142c9e09a2b4505
Shantanukpro <shantanukpro@gmail.com>
2025-09-17 08:22:39 +0530

    CRUD add for PC tables

--- LMS/labs/urls.py
+++ LMS/labs/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LabListView, LabCreateView, LabUpdateView, LabDeleteView, LabDetailView
+from .views import LabListView, LabCreateView, LabUpdateView, LabDeleteView, LabDetailView, PCCreateView, PCUpdateView, PCDeleteView
 
 urlpatterns = [
     path('', LabListView.as_view(), name='lab-list'),
@@ -7,4 +7,8 @@ urlpatterns = [
     path('<int:pk>/', LabDetailView.as_view(), name='lab-detail'),  # NEW
     path('<int:pk>/edit/', LabUpdateView.as_view(), name='lab-edit'),
     path('<int:pk>/delete/', LabDeleteView.as_view(), name='lab-delete'),
+        # PC CRUD
+    path('<int:lab_id>/pcs/add/', PCCreateView.as_view(), name='pc-add'),
+    path('pcs/<int:pk>/edit/', PCUpdateView.as_view(), name='pc-edit'),
+    path('pcs/<int:pk>/delete/', PCDeleteView.as_view(), name='pc-delete'),
 ]
--- LMS/labs/views.py
+++ LMS/labs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
-from .models import Lab
+from .models import Lab, PC
 from .forms import LabForm
 from django.urls import reverse_lazy
 
@@ -46,3 +46,42 @@ class LabDetailView(DetailView):
     model = Lab
     template_name = "labs/lab_detail.html"
     context_object_name = "lab"
+
+# ---------- PC CRUD ----------
+class PCCreateView(CreateView):
+    model = PC
+    template_name = "labs/pc_form.html"
+    fields = [
+        "asset_tag", "hostname", "serial_number", "ip_address", "mac_address",
+        "manufacturer", "model", "cpu", "cpu_cores", "ram_mb", "storage_gb",
+        "os_name", "os_version", "purchased_on", "warranty_until", "is_defective", "defective_reason"
+    ]
+
+    def form_valid(self, form):
+        lab = get_object_or_404(Lab, pk=self.kwargs["lab_id"])
+        form.instance.lab = lab
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy("lab-detail", kwargs={"pk": self.kwargs["lab_id"]})
+
+
+class PCUpdateView(UpdateView):
+    model = PC
+    template_name = "labs/pc_form.html"
+    fields = [
+        "asset_tag", "hostname", "serial_number", "ip_address", "mac_address",
+        "manufacturer", "model", "cpu", "cpu_cores", "ram_mb", "storage_gb",
+        "os_name", "os_version", "purchased_on", "warranty_until", "is_defective", "defective_reason"
+    ]
+
+    def get_success_url(self):
+        return reverse_lazy("lab-detail", kwargs={"pk": self.object.lab.pk})
+
+
+class PCDeleteView(DeleteView):
+    model = PC
+    template_name = "labs/pc_confirm_delete.html"
+
+    def get_success_url(self):
+        return reverse_lazy("lab-detail", kwargs={"pk": self.object.lab.pk})
